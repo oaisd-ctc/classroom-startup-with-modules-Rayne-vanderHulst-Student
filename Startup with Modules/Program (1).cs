31f25494454ceb5419da7d6ee429e3341970a956
Rayne <rava438@student.oaisd.org>
2024-10-16 08:52:11 -0400

    Startup with Modules
     Startup with Modules

---
+++ Startup with Modules/Program (1).cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Walk in the room.");
+        Console.WriteLine("Walk towards desk.");
+        Console.WriteLine("Pull out chair.");
+        Console.WriteLine("Take off backpack.");
+        Console.WriteLine("Place backpack on the ground.");
+        Console.WriteLine("Sit down in chair.");
+        Console.WriteLine("Turn on computer.");
+        Console.WriteLine("Sign in to computer.");
+        Console.WriteLine("Open browser.");
+        Console.WriteLine("Navigate to the Daily Logic Classroom.");
+        Console.WriteLine("Complete the Daily Logic for the day.");
+        Console.WriteLine("Inhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Exhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Inhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Exhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Inhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Exhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Inhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Exhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Inhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Exhale for 4 seconds.");
+        Console.WriteLine("Hold breath for 4 seconds.");
+        Console.WriteLine("Open a new tab in the browser.");
+        Console.WriteLine("Navigate to Google.");
+        Console.WriteLine("Open Gmail.");
+        Console.WriteLine("Read Daily Hypothetical.");
+        Console.WriteLine("Talk to neighbors about Daily Hypothetical.");
+        Console.WriteLine("Respond to Daily Hypothetical email.");
+        FindSeat();
+
+        SignIn();
+
+        DailyLogic();
+
+    }
+
+
+    public static void FindSeat()
+    {
+
+    }
+
+    public static void SignIn()
+    {
+
+    }
+
+    public static void DailyLogic()
+    {
+
+    }
+
+    public static void BoxBreathing()
+    {
+
+    }
+
+    public static void DailyHypotheticals()
+    {
+
+    }
+
+
+}
